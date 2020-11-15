5449b77dc7c98cc86d517649d4e9e6d023113986
fusionprint <72464148+fusionprint@users.noreply.github.com>
2020-11-15 16:39:42 -0300

    Update projeto2.py

--- projeto2.py
+++ projeto2.py
@@ -1,18 +1,19 @@
 import random
 
-print("#####Guess a number#####")
+def guess_a_number():
+    print("#####Guess a number#####")
 
-numero = random.randrange(1, 11)
+    numero = random.randrange(1, 11)
 
-chute = int(input(('Chute um número:')))
+    chute = int(input(('Chute um número:')))
 
-while (chute != numero):
-    if (chute > numero):
-        print("Seu chute foi alto")
-        chute=int(input("Tente de novo:"))
-    if (chute < numero):
-        print("Seu chute foi baixo")
-        chute=int(input("Tente de novo:"))
-else:
-    print("Você ganhou!")
-
+    while (chute != numero):
+        if (chute > numero):
+            print("Seu chute foi alto")
+            chute=int(input("Tente de novo:"))
+        if (chute < numero):
+            print("Seu chute foi baixo")
+            chute=int(input("Tente de novo:"))
+    else:
+        print("Você ganhou!")
+    guess_a_number()
