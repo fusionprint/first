d3c544d2581bbb2c602ba5e3efa48898fab9f08c
fusionprint <72464148+fusionprint@users.noreply.github.com>
2020-11-15 16:39:08 -0300

    Update projeto1.py

--- projeto1.py
+++ projeto1.py
@@ -1,13 +1,16 @@
 import random
 
-opcao=(input("Você gostaria de jogar o dado? (s) ou (n): "))
+def jogue_o_dado():
+    opcao=(input("Você gostaria de jogar o dado? (s) ou (n): "))
 
-while opcao == "s":
-    print(random.randrange(1, 7))
-    print("#####################")
-    opcao = str(input(("Você quer jogar de novo? (s) (n)")))
-else:
-    print('Ok bye')
+
+    while opcao == "s":
+        numero = random.randrange(1, 7)
+        print("Você tirou o número:", numero)
+        opcao = str(input(("Você quer jogar de novo? (s) ou (n)")))
+    else:
+        print('Ok bye')
+    jogue_o_dado()
 
 
 
